7dd2bd654ece5d25cdce0bd94ff3b087737a7252
Michael Yao <michaely2008@sina.com>
2018-11-16 11:36:59 +0800

    设置控制台缓存区大小

--- Program.cs
+++ Program.cs
@@ -55,6 +55,9 @@ namespace Zoro.RpcHost
         static void OnStart(string[] args)
         {
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            var bufferSize = 1024 * 67 + 128;
+            Stream inputStream = Console.OpenStandardInput(bufferSize);
+            Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, bufferSize));
 
             bool disableLog = false;
             for (int i = 0; i < args.Length; i++)
